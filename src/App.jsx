9c27781db69c213f21db95544906c34d3d8eeb7c
vsuri <surivansh555@gmail.com>
2026-02-04 22:46:30 +0530

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     try {
       // 3. Hit the Backend
       const endpoint = file ? 'upload' : 'chat';
-      const response = await fetch(`http://localhost:8000/${endpoint}`, {
+      const response = await fetch(`https://waste-segregation-i160.onrender.com/${endpoint}`, {
         method: 'POST',
         body: formData,
       });
